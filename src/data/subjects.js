147c4104f820889c41d706e02294f8f58e0a12f2
HARIVIGNESHRAO <harivigneshrao151@gmail.com>
2026-07-14 14:04:47 +0530

    dvt

--- src/data/subjects.js
+++ src/data/subjects.js
@@ -173,6 +173,8 @@ export const subjects1 = [
     icon: '⛏️',
     units: [
       { id: 'unit1', name: 'Unit 1',pdfFile:1 },
+      { id: 'unit2', name: 'Unit 2',pdfFile:2 },
+      { id: 'unit3', name: 'Unit 3',pdfFile:3 }
     ]
   },
 
