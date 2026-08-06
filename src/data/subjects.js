e70148fa3cdc1e50a72850904dbd7e4e68c3e1a0
HARIVIGNESHRAO <harivigneshrao151@gmail.com>
2026-08-06 10:39:38 +0530

    es

--- src/data/subjects.js
+++ src/data/subjects.js
@@ -377,7 +377,7 @@ export const syllabusSubjects1 = [
         name: 'Mid Exams Syllabus',
         topic: 'Mid Exams Syllabus',
         topics: '',
-        pdfFile: 'https://res.cloudinary.com/vunycbnq/image/upload/v1785824350/Mid_Exam_Syllabus_Updated_ydjxzp.pdf',
+        pdfFile: 'https://res.cloudinary.com/vunycbnq/image/upload/v1785992885/Mid_Exam_Syllabus_Updated_with_ES_Unit3_ndjhct.pdf',
       },
     ],
   },
