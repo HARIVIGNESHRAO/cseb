6ac86b796273d41f269106812e1caeee9632bc79
HARIVIGNESHRAO <harivigneshrao151@gmail.com>
2026-08-21 11:38:15 +0530

    dm

--- src/data/subjects.js
+++ src/data/subjects.js
@@ -178,7 +178,6 @@ export const subjects1 = [
       { id: 'unit1', name: 'Unit 1', pdfFile: 'DM_UNIT_1' },
       { id: 'unit2', name: 'Unit 2', pdfFile: 'DM_UNIT_2' },
       { id: 'unit3', name: 'Unit 3', pdfFile: 'DM_UNIT_3' },
-      { id: 'unitp', name: 'Unit 3 problems (for your reference)', pdfFile: 'p' },
       { id: 'unity', name: 'Youtube Links', pdfFile: 'https://docs.google.com/spreadsheets/d/1j-CPk1Cewu4zVhnw8Ce8eFcPZq4vQ-xVL7wcq4wy6Ag/edit?gid=0#gid=0' },
       { id: 'unitn', name: 'Important Topics', pdfFile: 'i' }
     ]
